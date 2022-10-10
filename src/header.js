5d649e77545134b5833120354d0065d6d792fb66
Kevin Sarria <reysabueso256@gmail.com>
2022-10-10 01:06:31 -0500

    modificaciones: se mejoro el menu responsive, las animaciones basicas del menu y se agrego un boton de volvere arriba, ademas se pulio el tema del scroll cuando navega entre secciones

--- src/header.js
+++ src/header.js
@@ -1,23 +1,63 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    
     const header = document.querySelector('.header');
+    const menuBurger = document.querySelector('.menu-burger');
+    const barraLinks = document.querySelector('.navegacion');
+    const links = document.querySelectorAll('.link-navegacion');
+    const botonVolverArriba = document.querySelector('.boton-volver-arriba');
 
 
     window.onscroll = () => {
         
-        let y = window.scrollY;
+        let positionY = window.scrollY;
 
-        if( y > 180 ) {
+        if( positionY > 180 ) {
 
             header.classList.add('bg-slate-900');
             header.classList.remove('bg-transparent');
-        
-        } else if( y <= 179 ) {
+            header.classList.add('border-b-4', 'border-b-slate-700');
+
+        } else if( positionY <= 179 ) {
             header.classList.add('bg-transparent');
             header.classList.remove('bg-slate-900');
+            header.classList.remove('border-b-4', 'border-b-slate-700');
+        }
+
+
+        if( positionY >= 677 ) {
+            botonVolverArriba.classList.add('block');
+        }else {
+            botonVolverArriba.classList.remove('block');
         }
 
     }
+
+    menuBurger.addEventListener('click', () => {
+
+        menuBurger.classList.toggle('active');
+        barraLinks.classList.toggle('active-menu');
+
+
+    });
+
+    // Este codigo verifica si se le dio click a un link de la barra de navegacion
+    links.forEach( link => hizoClick(link) )
+   
+    // Esta function hace que el menu vuelva a su estado comprimido en el modo pantalla pequeña si se da click sobre un link de la barra de navegacion
+    function hizoClick( boton ) {
+
+        boton.addEventListener('click', () => {
+
+            if( menuBurger.classList.contains('active') ) {
+                menuBurger.classList.toggle('active');
+                barraLinks.classList.toggle('active-menu');
+            }
+
+        })
+
+    }
+
 
 });
